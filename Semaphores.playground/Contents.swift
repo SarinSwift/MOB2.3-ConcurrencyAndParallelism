9f3273244dc4f10223475c9aaf684bee4060137b
SarinSwift <sarinyaswift@gmail.com>
2019-06-11 17:19:19 -0700

    finished semaphores function

--- Semaphores.playground/Contents.swift
+++ Semaphores.playground/Contents.swift
@@ -20,17 +20,35 @@ DispatchQueue.global().async {
     print("Person 2 - done with Switch")
     semaphore.signal()
 }
+DispatchQueue.global().async {
+    print("Person 3 - wait")
+    semaphore.wait()
+    print("Person 3 - wait finished")
+    sleep(1) // Person 3 playing with Switch
+    print("Person 3 - done with Switch")
+    semaphore.signal()
+}
 
 
 func downloadMovies(numberOfMovies: Int) {
     
     // Create a semaphore
+    let semaphore = DispatchSemaphore(value: 1)
     
-    // Launch 8 tasks (8 movie downloads)
-    // Each task should wait (pretend downloading takes 2 seconds) and inform the console once it's done(call signal).
     // Run the tasks on a background thread.
-    // Let the semaphore know when you release the resource
-    
+    DispatchQueue.global(qos: .background).async {
+        // Launch 8 tasks (8 movie downloads)
+        // Each task should wait (pretend downloading takes 2 seconds) and inform the console once it's done(call signal).
+        for i in 1...8 {
+            semaphore.wait()
+            print("launching task \(i)")
+            sleep(1)
+            // Let the semaphore know when you release the resource
+            semaphore.signal()
+        }
+        
+        print("Completed all resources")
+    }
 }
 
 downloadMovies(numberOfMovies:2)
